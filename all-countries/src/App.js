080e4baa9479ac51e43b58ee207dc8ebd3b1d51a
Volodymyr <syvochka64@gmail.com>
2021-03-31 19:04:12 +0200

    Added quiz 1

--- all-countries/src/App.js
+++ all-countries/src/App.js
@@ -62,19 +62,23 @@ const Home = () => {
     return (
       <div className="container">
         
-        <div className="header">
-          <div className="header-info">
-            <h1>Where in the world?</h1>
-            <button className="quiz-btn">
-              <Link to="/quiz">
-                <span>
-                  <i className="fas fa-address-book-o"></i>
-                </span>
-                <span>Quiz</span>
-              </Link>
-            </button>
-          </div>
-        </div>
+        <button className="quiz1-btn">
+          <Link to="/quiz1">
+            <span>
+              <i className="fas fa-address-book-o"></i>
+            </span>
+            <span>Quiz 1</span>
+          </Link>
+        </button>
+    
+        <button className="quiz2-btn">
+          <Link to="/quiz2">
+            <span>
+              <i className="fas fa-address-book-o"></i>
+            </span>
+            <span>Quiz 2</span>
+          </Link>
+        </button>
         
         <div className="filter">
           <div className="search-bar">
@@ -171,26 +175,28 @@ const Home = () => {
       <Route path="/:name">
         <SingleCountry data={data} />
       </Route>
-      <Route exact path="/quiz">
-        <Quiz data={data} />
+      <Route exact path="/quiz1">
+        <Quiz1 data={data} />
       </Route>
-      <Route exact path="/quizReload">
-        <QuizReload data={data} />
+      <Route exact path="/quiz1Reload">
+        <Quiz1Reload data={data} />
+      </Route>
+      <Route exact path="/quiz2">
+        <Quiz2 data={data} />
+      </Route>
+      <Route exact path="/quiz2Reload">
+        <Quiz2Reload data={data} />
       </Route>
     </Router>
   );
 }
 
-const QuizReload = ({ data }) => {
+const Quiz1Reload = ({ data }) => {
   let history = useHistory();
   
   useEffect(
     () => {
-      let timer1 = setTimeout(() => { history.push("/quiz"); }, 500);
-
-      // this will clear Timeout
-      // when component unmount like in willComponentUnmount
-      // and show will not change to true
+      let timer1 = setTimeout(() => { history.push("/quiz1"); }, 500);
       return () => {
         clearTimeout(timer1);
       };
@@ -206,7 +212,168 @@ const QuizReload = ({ data }) => {
   return (<></>)
 }
 
-const Quiz = ({ data }) => {
+const Quiz1 = ({ data }) => {
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  
+  const [item, setItem] = useState(data[Math.floor(Math.random() * data.length)]);
+  
+  const [capital, setCapital] = useState(item.capital);
+  
+  const [inputCapital, setInputCapital] = useState("");
+  
+  const [capitalCorrect, setCapitalCorrect] = useState(false);
+  
+  const checkIfCorrect = () => {
+    if (inputCapital === capital) {
+      setCapitalCorrect(true);
+      var audio = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3');
+      audio.play();
+    }
+    else {
+      setCapitalCorrect(false);
+      setInputCapital(capital);
+    }
+  };
+  
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      checkIfCorrect();
+    }
+  };
+  
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <div className="header-info">
+            <h1>Where in the world?</h1>
+          </div>
+        </div>
+        <div className="custom-cont">
+          <div>
+            <button className="back-btn">
+              <Link to="/">
+                <span>
+                  <i className="fas fa-arrow-left"></i>
+                </span>
+                <span>Back</span>
+              </Link>
+            </button>
+            <button className="next-btn">
+              <Link to="/quiz1Reload">
+                <span>
+                  <i className="fas fa-arrow-right"></i>
+                </span>
+                <span>Next</span>
+              </Link>
+            </button>
+          </div>
+          <div className="input-capital">
+            <i className="fas fa-search"></i>
+            <input
+              type="text"
+              placeholder="Capital..."
+              value={inputCapital}
+              autoFocus
+              onChange={(e) => {
+                setInputCapital(e.target.value);
+              }}
+              onKeyPress={handleKeyPress}
+            />
+          </div>
+          <div className="custom-grid">
+            <div className="custom-img">
+              <img src={item.flag} alt="img" />
+            </div>
+            <div className="custom-text">
+              <h2>{item.name}</h2>
+              <div className="tc">
+                <div className="1">
+                  <h5>
+                    <span className="bold">Native Name: </span>
+                    <span className="light">{item.nativeName}</span>
+                  </h5>
+                  <h5>
+                    <span className="bold">Population: </span>
+                    <span className="light">
+                      {numberWithCommas(item.population)}
+                    </span>
+                  </h5>
+                  <h5>
+                    <span className="bold">Region: </span>
+                    <span className="light">{item.region}</span>
+                  </h5>
+                  <h5>
+                    <span className="bold">Sub Region: </span>
+                    <span className="light">{item.subregion}</span>
+                  </h5>
+                </div>
+                <div className="2">
+                  <h5>
+                    <span className="bold">Top Level Domain: </span>
+                    <span className="light">{item.topLevelDomain}</span>
+                  </h5>
+                  <h5>
+                    <span className="bold">Currencies: </span>
+                    <span className="light">
+                      {item.currencies[0].name}
+                    </span>
+                  </h5>
+                  <h5>
+                    <span className="bold">Languages: </span>
+                    <span className="light">
+                      {item.languages.map((r) => r.name).join(", ")}
+                    </span>
+                  </h5>
+                </div>
+              </div>
+              <div className="border">
+                <h4>Border Countries: </h4>
+                <span>
+                  {item.borders.map((f, index) => {
+                    if (!f) {
+                      return <p>No Bordering Countries</p>;
+                    }
+                    return (
+                      <button className="border-btn">
+                        {f}
+                      </button>
+                    );
+                  })}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const Quiz2Reload = ({ data }) => {
+  let history = useHistory();
+  
+  useEffect(
+    () => {
+      let timer1 = setTimeout(() => { history.push("/quiz2"); }, 500);
+      return () => {
+        clearTimeout(timer1);
+      };
+    },
+    // useEffect will run only one time with empty []
+    // if you pass a value to array,
+    // like this - [data]
+    // than clearTimeout will run every time
+    // this value changes (useEffect re-run)
+    []
+  );
+
+  return (<></>)
+}
+
+const Quiz2 = ({ data }) => {
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
@@ -234,7 +401,7 @@ const Quiz = ({ data }) => {
               </Link>
             </button>
             <button className="next-btn">
-              <Link to="/quizReload">
+              <Link to="/quiz2Reload">
                 <span>
                   <i className="fas fa-arrow-right"></i>
                 </span>
